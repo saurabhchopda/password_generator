464e174c4f98ea88064dc566b0cdb0f291655bda
Saurabh Chopda <saurabhchopda1997@gmail.com>
2020-09-14 23:33:03 +0530

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -1,17 +1,16 @@
 # Random Password Generator
 
 import random
+import sys
 
-characters = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890@$'
+def passgen(length_of_pass):
+	characters = 'QWERTYUIOPASDFGHJKLZXCVBNM1234567890@$qwertyuiopasdfghjklzxcvbnm'
 
-password = ''
+	password = ''
 
-print('Enter the length of the password :')
+	for c in range(0, length_of_pass):
+		password += random.choice(characters)
+		
+	return password	
 
-length_of_pass = int(input())
-
-
-for c in range(0, length_of_pass):
-    password += random.choice(characters)
-
-print(password)
+print(passgen(int(sys.argv[1])))
